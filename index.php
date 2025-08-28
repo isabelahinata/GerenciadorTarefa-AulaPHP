5ba7952ef8cbf667afe670f5c863501cd55ce8c6
ISABELA CAMARGO <isabela.4977@aluno.pr.senac.br>
2025-08-28 10:56:17 -0300

    Criando tela de login,criando botao de logout com icone

--- index.php
+++ index.php
@@ -18,6 +18,11 @@
         <nav class="navbar navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="index.php">Crud PHP</a>
+                <form action="login.php" method="$_GET" class="d-inline">
+                    <button type="submit" class="btn btn-secondary">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </button>
+                </form>
             </div>
         </nav>
         <main class="container p-4">
@@ -28,7 +33,7 @@
                         <form action="save.php" method="POST">
                             <div class="form-group">
                                 <input type="text" name="title" class="form-control"
-                                    placeholder="Titulo da tarefa " autofocus>
+                                    placeholder="Titulo da tarefa " autofocus required>
                             </div>
                             <div class="form-group">
                                 <textarea name="description" rows="2" class="form-control"
@@ -67,7 +72,7 @@
                                     <tr>
                                         <td><?php echo $row['id']; ?></td>
                                         <td><?php echo $row['title']; ?></td>
-                                        <td><?php echo $row['description']; ?></td>
+                                        <td><?= substr($row['description'], 0, 20) . '...' ?></td>
                                         <td><?=date("d/m/Y", strtotime($row['created_at'])) ; ?></td>
                                         <td><a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-secondary">
                                                 <i class="fas fa-marker"></i>
